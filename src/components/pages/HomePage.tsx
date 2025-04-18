2eb516f4ec27fee810bba4cbc87647b55105fd39
Paulius <p.jacikeviciusinfo@gmail.com>
2025-04-18 07:33:28 +0300

    HomePage cleanup

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,18 @@
 import { useContext, useEffect } from "react";
 import FooterContext from "../../contexts/FoooterContext";
+import styled from "styled-components";
+
+
+const Home = styled.div`
+    
+    background: linear-gradient(180deg, #0f2027, #203a43, #2c5364);
+    height: 50vh;
+    >p 
+    {
+        margin: 0;
+        padding: 15px;
+    }
+`;
 
 const HomePage = () => {
 
@@ -11,6 +24,10 @@ const HomePage = () => {
     
     return (  
         <>
+        <Home>
+         <p>Home</p>
+        </Home>
+       
         </>
     );
 }
